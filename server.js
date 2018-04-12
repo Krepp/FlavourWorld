6f5e667d7bae7239bd1c87383fe152801a43f961
ZOrvis <a.j.orvis@rgu.ac.uk>
2018-04-12 10:04:13 +0100

    trying to get it to work

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const bodyParser = require('body-parser')
 const app = express();
 
 
-app.use(bodyParser.urlencoded({extended: true}))
+// app.use(bodyParser.urlencoded({extended: true}))
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 app.get('/', function(req,res) {
-    res.render('pages/index')
+    res.render('pages/home')
 });
