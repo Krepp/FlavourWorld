36f0b0633209aa210e1677b62196d877ea8fedcf
ZOrvis <a.j.orvis@rgu.ac.uk>
2018-04-12 09:54:41 +0100

    First Commit

---
+++ server.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+
+
+app.use(bodyParser.urlencoded({extended: true}))
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.get('/', function(req,res) {
+    res.render('pages/index')
+});
