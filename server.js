1153871e2ceb292d13909a472023bd4e9522eb51
ZOrvis <a.j.orvis@rgu.ac.uk>
2018-04-12 10:20:38 +0100

    Another attempt to get server working

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-
-const express = require('express');
-// const bodyParser = require('body-parser')
-const app = express();
-
-
-// app.use(bodyParser.urlencoded({extended: true}))
+// server.js
+// load the things we need
+var express = require('express');
+var app = express();
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-
-app.get('/', function(req,res) {
-    res.render('pages/index')
+// use res.render to load up an ejs view file
+// index page
+app.get('/', function(req, res) {
+ res.render('pages/index');
 });
+// about page
+app.get('/about', function(req, res) {
+ res.render('pages/about');
+});
+app.listen(8080);
+console.log('8080 is the magic port');
