34cc5318a47d81ec0bba8c8470386092c295dab1
ZOrvis <a.j.orvis@rgu.ac.uk>
2018-04-12 10:33:17 +0100

    Placing css in public folder, may not work

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // load the things we need
 var express = require('express');
 var app = express();
+app.use(express.static('public'))
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
